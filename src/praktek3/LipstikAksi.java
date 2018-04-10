7dfa38025edb8b796fec8abe18c7bb84edb4361e
olgaputri <olgaputri654321@gmail.com>
2018-04-10 13:51:21 +0700

    Menggunakan Method Set

--- src/praktek3/LipstikAksi.java
+++ src/praktek3/LipstikAksi.java
@@ -2,9 +2,10 @@ package praktek3;
 public class LipstikAksi {
     public static void main (String[] args){
         Lipstik c = new Lipstik();
-        c.merk="bodyshop";
-        c.warna="pink";
-        c.harga="150000";
+       
+        c.setMerk("Bodyshop");
+        c.setWarna("pink");
+        c.setHarga("150000");
         
         c.cetakinfo();
               
