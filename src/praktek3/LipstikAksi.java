e32901d2fe184a9491eaedba4a76f16f9bbc16fe
olgaputri <olgaputri654321@gmail.com>
2018-04-10 14:04:31 +0700

    Menggunakan Method Get

--- src/praktek3/LipstikAksi.java
+++ src/praktek3/LipstikAksi.java
@@ -8,6 +8,13 @@ public class LipstikAksi {
         c.setHarga("150000");
         
         c.cetakinfo();
+        
+        System.out.println("Merknya \t: ");
+        System.out.println(c.getMerk());
+        System.out.println("Warnanya\t: ");
+        System.out.println(c.getWarna());
+        System.out.println("Harganya \t: ");
+        System.out.println(c.getHarga());
               
     }
     
