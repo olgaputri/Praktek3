5c315a5b59dcc9a70d41b9922beb244e8ac64771
olgaputri <olgaputri654321@gmail.com>
2018-04-10 13:05:58 +0700

    Membuat Atribut

--- src/praktek3/Lipstik.java
+++ src/praktek3/Lipstik.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package praktek3;
 
-/**
- *
- * @author proWindows 10
- */
 public class Lipstik {
+    String merk;
+    String warna;
+    String harga;
+    
+    
     
 }
