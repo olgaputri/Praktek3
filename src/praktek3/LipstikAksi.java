1c0fd118e1c423d7cf49f1a51d22160593940c1f
olgaputri <olgaputri654321@gmail.com>
2018-04-10 13:35:38 +0700

    Instance, buat atribut dan panggil method

--- src/praktek3/LipstikAksi.java
+++ src/praktek3/LipstikAksi.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package praktek3;
-
-/**
- *
- * @author proWindows 10
- */
 public class LipstikAksi {
+    public static void main (String[] args){
+        Lipstik c = new Lipstik();
+        c.merk="bodyshop";
+        c.warna="pink";
+        c.harga="150000";
+        
+        c.cetakinfo();
+              
+    }
     
 }
